65b35d87174be496b5bd27e68cda387cb8980bed
jueunk617 <juuuune62@naver.com>
2025-07-02 10:29:34 +0900

    글쓰기 작성 + apiFetch 사용

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -24,6 +24,10 @@ export default function Page() {
             </li>
           ))}
         </ul>
+
+        <div>
+          <Link href="/posts/write">글쓰기</Link>
+        </div>
       </>
     );
   }
